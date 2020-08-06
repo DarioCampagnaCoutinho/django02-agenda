03cce71481a4f03ffed8475d760c3aeece487401
Dario Campagna <dario@localhost.localdomain>
2020-08-06 16:07:38 -0300

    Alterando os campos da classe Contato.

--- contato/models.py
+++ contato/models.py
@@ -3,17 +3,17 @@ from django.utils import timezone
 
 
 class Categoria(models.Model):
-    nome = models.CharField(max_length=60)
+    nome = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome
 
 
 class Contato(models.Model):
-    nome = models.CharField(max_length=100)
-    sobrenome = models.CharField(max_length=100, blank=True)
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=250, blank=True)
     telefone = models.CharField(max_length=22)
-    email = models.CharField(max_length=120, blank=True)
+    email = models.CharField(max_length=250, blank=True)
     data_criacao = models.DateTimeField(default=timezone)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)

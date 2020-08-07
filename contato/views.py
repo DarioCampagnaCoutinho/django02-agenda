3aa59deefababe5449dd5b1c7bb8dc225fa7216b
Dario Campagna <dario@localhost.localdomain>
2020-08-07 17:38:11 -0300

    Ordenando e filtrando os valores

--- contato/views.py
+++ contato/views.py
@@ -1,10 +1,11 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('nome').filter(mostrar=True)
     paginator = Paginator(contatos, 3)
     page = request.GET.get('p')
     contatos = paginator.get_page(page)
@@ -16,6 +17,8 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+    if not contato.mostrar:
+        raise Http404()
     contexto = {
         'contato': contato,
     }
